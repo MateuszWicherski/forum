112a09d14e6d8c00b8b5a843fd1bd1d6de8794ac
Mateusz Wicherski <mateusz.wicherski@tratif.com>
2020-04-14 20:18:43 +0200

    Initial commit

---
+++ src/main/java/pl/wicherski/forum/engine/ForumEngineApplication.java
@@ -0,0 +1,13 @@
+package pl.wicherski.forum.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wicherski/forum/engine/ForumEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wicherski.forum.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
